78f31caa1b3a8b9d8d51e135da681798f01e43bd
Michał Andrzejewski <36901567+michal-andrzejewski@users.noreply.github.com>
2019-04-20 19:32:23 +0200

    adding test add
    added test add (banner) using AdMob

--- FootballScoreApp/app/src/main/java/com/example/FootballScoreApp/MainActivity.kt
+++ FootballScoreApp/app/src/main/java/com/example/FootballScoreApp/MainActivity.kt
@@ -4,14 +4,25 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        MobileAds.initialize(this, "ca-app-pub-9929957078189954~5526922297")
+
         rViewMainMatches.layoutManager = LinearLayoutManager(this)
         rViewMainMatches.adapter = MainMatchesAdapter()
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
